f26a6a733f97933b9690d66189f10cb373a11919
BeYkeRYkt <beykerykt@gmail.com>
2017-05-11 16:39:41 +0700

    marmite: add product init.mk

---
+++ product/init.mk
@@ -0,0 +1,10 @@
+# Init scripts
+PRODUCT_PACKAGES += \
+    init.qcom.bt.sh
+
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    init.qcom.rc \
+    init.qcom.sh \
+    init.recovery.qcom.rc \
+    ueventd.qcom.rc
