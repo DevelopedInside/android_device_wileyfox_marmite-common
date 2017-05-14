fe53fcb784571ddce42e92cbac088288614c9bba
BeYkeRYkt <beykerykt@gmail.com>
2017-05-15 00:57:19 +0900

    marmite: disable cyanogen hardware (?)

--- board/cyanogen_hardware.mk
+++ board/cyanogen_hardware.mk
@@ -1,4 +1,4 @@
 # CM Hardware
-BOARD_USES_CYANOGEN_HARDWARE := true
-BOARD_HARDWARE_CLASS += \
-    hardware/cyanogen/cmhw
+#BOARD_USES_CYANOGEN_HARDWARE := true
+#BOARD_HARDWARE_CLASS += \
+#    hardware/cyanogen/cmhw
