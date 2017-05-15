4258a3056f57d428191ade5359560e288d4ebaa3
BeYkeRYkt <beykerykt@gmail.com>
2017-05-15 22:57:18 +0900

    marmite: commit consumerir

--- product/consumerir.mk
+++ product/consumerir.mk
@@ -1,6 +1,6 @@
 # IR
-PRODUCT_PACKAGES += \
-    consumerir.msm8937
+#PRODUCT_PACKAGES += \
+#    consumerir.msm8937
 
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml
+#PRODUCT_COPY_FILES += \
+#    frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml
