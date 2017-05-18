54e8ecfddbe8b1de8e7e096f6ae4d397f9b2f37e
BeYkeRYkt <beykerykt@gmail.com>
2017-05-19 04:07:03 +0900

    marmite: revert "marmite: remove unnecessary links"

--- Android.mk
+++ Android.mk
@@ -24,9 +24,9 @@ include $(CLEAR_VARS)
 
 # Create a link for the WCNSS config file, which ends up as a writable
 # version in /data/misc/wifi
-#$(shell mkdir -p $(TARGET_OUT)/etc/firmware/wlan/prima; \
-#    ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
-#	    $(TARGET_OUT)/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
+$(shell mkdir -p $(TARGET_OUT)/etc/firmware/wlan/prima; \
+    ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
+	    $(TARGET_OUT)/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
 
 #Create symbolic links
 #$(shell mkdir -p $(TARGET_OUT)/vendor/lib; \
